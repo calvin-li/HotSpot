3b0c9e845b54ab01184f500170444d36d1f06a8a
Calvin Li <calvinliqh@gmail.com>
2020-04-15 00:06:06 -0700

    initial commit

---
+++ HotSpot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+using Windows.Networking.NetworkOperators;
+
+namespace HotSpot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var profile = NetworkInformation.GetInternetConnectionProfile();
+                var tetheringManager = NetworkOperatorTetheringManager.CreateFromConnectionProfile(profile);
+                if (tetheringManager.TetheringOperationalState == TetheringOperationalState.Off)
+                {
+                    TurnOnHotSpot(tetheringManager).Wait();
+                }
+                System.Threading.Thread.Sleep(5000);
+            }
+
+            return;
+        }
+
+        private static async Task TurnOnHotSpot(NetworkOperatorTetheringManager tetheringManager)
+        {
+            await tetheringManager.StartTetheringAsync();
+        }
+    }
+}
